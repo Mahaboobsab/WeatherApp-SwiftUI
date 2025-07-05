b98cb1e8b02fa4ad2a08608597a2088bae6f6146
Mahaboobsab <git config --global user.email YourEmailProvidedToGit@Somedomain.com>
2025-07-05 22:59:15 +0530

    Initial Commit

---
+++ FigmaClone/FigmaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaCloneApp.swift
+//  FigmaClone
+//
+//  Created by Alkit Gupta on 05/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FigmaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
